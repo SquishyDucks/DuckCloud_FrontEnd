065323b5cded8faae7b17a6cba0d0cb6a6a3e775
tazpowell <hellotazuko@gmail.com>
2018-07-18 10:42:49 -0400

    Hide update/delete buttons if not owner
    - createFileTable function now also calls checkFileOwnership, which
      checks if owner id on file-row matches the current user's id. If
      owner id on file-row is empty or does not match, it will add 'hide'
      class to the update and delete buttons.

--- assets/scripts/upload/ui.js
+++ assets/scripts/upload/ui.js
@@ -1,12 +1,38 @@
 'use strict'
-
+const store = require('../store')
 const showFilesTemplate = require('../templates/file_row.handlebars')
+
+const checkFileOwnership = function (data) {
+  console.log('data is ', data)
+  console.log('data-owner is ', data.getAttribute('data-owner'))
+  if (data.getAttribute('data-owner') === '') {
+    data.querySelectorAll('.clickable').forEach((x) => { x.classList.add('hide') })
+    // data.querySelector('delete-btn').classList.add('hide')
+  } else if (data.getAttribute('data-owner') === store.user._id) {
+  } else {
+    data.querySelectorAll('.clickable').forEach((x) => { x.classList.add('hide') })
+    // data.querySelector('delete-btn').classList.add('hide')
+  }
+  // debugger
+}
 
 const createFileTable = function (data) {
   // console.log('data inside createFileTable is ', data)
+  // generate a table row for each file using handlebars
   const showFilesHtml = showFilesTemplate({ uploads: data.uploads })
   // console.log('showFilesHtml is ', showFilesHtml)
+  // add the generated table rows inside the table body element
   $('#file-table-body').html(showFilesHtml)
+  // find all table row elements using class 'file-row'
+  const filesCreated = document.getElementsByClassName('file-row')
+  console.log('filesCreated is ', filesCreated)
+  // converts htmlCollection into array
+  const arr = Array.prototype.slice.call(filesCreated)
+  console.log('arr is ', arr)
+  arr.forEach(function (x) {
+    console.log('x is ', x)
+    checkFileOwnership(x)
+  })
 }
 
 const uploadFileSuccess = function (uploadFileResponse) {
