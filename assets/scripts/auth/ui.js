c6e12c69298efde7a55827fde4ec5350a0e1631a
arthur85c <arthur85c@gmail.com>
2018-07-18 16:49:20 -0400

    Added a preliminary Nav bar

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -16,6 +16,11 @@ const signInSuccess = function (signInResponse) {
   $('.navbar-brand').show(500)
   uploadAPI.getUploads()
     .then((data) => uploadUi.createFileTable(data))
+  $('.navbar-nav').append(`
+      <li class="nav-item active">
+        <button id= 'sign-out-button' class='auth auth-button' style="display: none;">Sign Out</button>
+      </li>
+    `)
 }
 
 const signInFail = function () {
