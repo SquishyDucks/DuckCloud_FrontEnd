77ba51bf125089ec0ad42929c5a1780b89446e0c
howardreith <38789146+howardreith@users.noreply.github.com>
2018-07-19 13:19:18 -0400

    Update ui.js

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -61,7 +61,7 @@ const signUpFailPW = function () {
   $('.alerts').html(`
     <div class="alert alert-danger alert-dismissible" role="alert">
       <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      <strong>Sign up failed!</strong> Your password and password confirmation does not match.
+      <strong>Sign up failed!</strong> Your password and password confirmation do not match.
     </div>
     `)
   $('.alert').delay(2500).fadeOut()
