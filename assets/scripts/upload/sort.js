dd32a5ec45c3c39a8434016d17408a8cce80b3c9
Howard Reith <howard.reith@gmail.com>
2018-07-30 12:59:30 -0400

    Dates now sort by most recent
    -Clicking on the date headings will now sort such that the most recent
    ones are at the top, not the least recent.
    -Removed the instructional comments.

--- assets/scripts/upload/sort.js
+++ assets/scripts/upload/sort.js
@@ -15,34 +15,29 @@ const onSortTags = function () {
 
 const onSortTable = function (heading) {
   console.log('a heading was clicked!')
-  let table, rows, switching, i, x, y, shouldSwitch
-  table = document.getElementById('the-table')
-  switching = true
-  /* Make a loop that will continue until
-  no switching has been done: */
+  let rows, i, x, y, shouldSwitch
+  const table = document.getElementById('the-table')
+  let switching = true
   while (switching) {
-    // Start by saying: no switching is done:
     switching = false
     rows = table.getElementsByTagName('TR')
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
     for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
       shouldSwitch = false
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
       x = rows[i].getElementsByTagName('TD')[heading]
       y = rows[i + 1].getElementsByTagName('TD')[heading]
-      // Check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        // If so, mark as a switch and break the loop:
-        shouldSwitch = true
-        break
+      if (heading === 0 || heading === 3) {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true
+          break
+        }
+      } else {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true
+          break
+        }
       }
     }
     if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
       switching = true
     }
